146169ac98d5c7ac9efbc6a8454e3cee120b0f36
sharkowo64 <214218@clvsc.tyc.edu.tw>
2025-02-13 14:38:45 +0800

    debug

--- app.py
+++ app.py
@@ -16,27 +16,27 @@ line_handler = WebhookHandler(LINE_CHANNEL_SECRET)
 def home():
     return "owo"
 
-@app.route("/callback", methods=["POST"])
-def callback():
-    signature = request.headers["X-Line-Signature"]
-    body = request.get_data(as_text=True)
+# @app.route("/callback", methods=["POST"])
+# def callback():
+#     signature = request.headers["X-Line-Signature"]
+#     body = request.get_data(as_text=True)
 
-    try:
-        line_handler.handle(body, signature)
-    except InvalidSignatureError:
-        abort(400)
+#     try:
+#         line_handler.handle(body, signature)
+#     except InvalidSignatureError:
+#         abort(400)
 
-    return "OK"
+#     return "OK"
 
-@line_handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-    user_message = event.message.text
-    reply_message = f"你說了：{user_message}"
+# @line_handler.add(MessageEvent, message=TextMessage)
+# def handle_message(event):
+#     user_message = event.message.text
+#     reply_message = f"你說了：{user_message}"
     
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=reply_message)
-    )
+#     line_bot_api.reply_message(
+#         event.reply_token,
+#         TextSendMessage(text=reply_message)
+#     )
 
-if __name__ == "__main__":
-    app.run(port=8000)
+# if __name__ == "__main__":
+#     app.run(port=8000)
