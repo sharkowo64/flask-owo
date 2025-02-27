7804d851e67439365cb1b5cfc535a7f875c86fa8
sharkowo64 <214218@clvsc.tyc.edu.tw>
2025-02-27 14:19:04 +0800

    V0.4.0

--- app.py
+++ app.py
@@ -2,11 +2,13 @@ from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
+from openai import OpenAI
 import os
 
 app = Flask(__name__)
 
 # 設定 LINE Channel Access Token 和 Secret
+
 LINE_CHANNEL_ACCESS_TOKEN = os.getenv("LINE_CHANNEL_ACCESS_TOKEN")
 LINE_CHANNEL_SECRET = os.getenv("LINE_CHANNEL_SECRET")
 
@@ -31,7 +33,21 @@ def callback():
 @line_handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
      user_message = event.message.text
-     reply_message = f"你說了：{user_message}"
+
+     client = OpenAI(api_key = os.getenv("OPENAI_KEY"))
+
+     completion = client.chat.completions.create(
+          model="gpt-4o-mini",
+          messages=[
+               {"role": "system", "content": "你是一個智能機器人管家"},
+               {
+                    "role": "user",
+                    "content":input()
+               }
+          ]
+     )
+
+     reply_message = completion.choices[0].message.content
     
      line_bot_api.reply_message(
          event.reply_token,
